f817b9fe57802ba75a3d10c6ef2e67d2a7fce65b
aichelindavid@gmail.com <aichelindavid@gmail.com>
2024-12-12 14:45:46 +0000

    change reservations add channel

--- models/intermediate/f_reservations.sql
+++ models/intermediate/f_reservations.sql
@@ -1,5 +1,3 @@
-
-
 select 
 RESERVATION_UUID as reservation_id,
 CUSTOMER_UUID as customer_id,
@@ -13,5 +11,4 @@ PARTY_SIZE as party_size,
 IS_WALK_IN as is_walk_in,
 LUNCH_TYPE as lunch_type,
 from thefork.stg_reservation r
-left outer join thefork.d_channels c on r.CHANNEL = c.channel 
-
+left outer join thefork.d_channels c on r.CHANNEL = c.channel
