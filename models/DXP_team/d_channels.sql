d0412d3b174636846f2b0bf9ccf5594ab18c5b1f
aichelindavid@gmail.com <aichelindavid@gmail.com>
2024-12-16 14:51:21 +0000

    add coments

--- models/DXP_team/d_channels.sql
+++ models/DXP_team/d_channels.sql
@@ -1,11 +1,16 @@
+-- pipelines for d_channels
+-- David Aichelin - 16/12/2024
+-- This code populates the d_channel table.
+
 {{
     config(
         materialized='table',
         unique_key='channel_id'
     )
 }}
+
 SELECT 
-distinct DENSE_RANK() OVER (ORDER BY channel) AS channel_id,
-CHANNEL as channel_name
-from thefork.stg_reservation  
-ORDER BY CHANNEL
+    distinct DENSE_RANK() OVER (ORDER BY channel)       AS channel_id,
+    CHANNEL                                             AS channel_name
+from {{ ref('stg_reservation') }}
+ORDER BY channel
--- models/DXP_team/d_dates.sql
+++ models/DXP_team/d_dates.sql
@@ -1,23 +1,30 @@
+-- pipelines for d_dates
+-- David Aichelin - 16/12/2024
+-- This code populates the d_dates table.
+
 {{
     config(
         materialized='table',
         unique_key='date_id'
     )
 }}
+
+-- We retrieve all the dates present in the reservations table.
 with all_date as (
-select distinct date from ( 
-  SELECT
-  distinct DT_DAY_BOOKING_DATE as date from thefork.stg_reservation
-  union all
-  SElect distinct DT_DAY_MEAL_DATE as date from thefork.stg_reservation
-)
-)
+select 
+  distinct date 
+from ( 
+    SELECT
+    distinct DT_DAY_BOOKING_DATE as date from {{ ref('stg_reservation') }}
+    union all
+    SElect distinct DT_DAY_MEAL_DATE as date from {{ ref('stg_reservation') }}
+))
 
 select 
-FORMAT_DATE('%Y%m%d', date) as date_id,
-date,
-FORMAT_DATE('%Y', date) as year,
-FORMAT_DATE('%m', date) as month,
-FORMAT_DATE('%d', date) as day,
-FORMAT_DATE('%A' , date) as weekday,
+    FORMAT_DATE('%Y%m%d', date)     as date_id,
+    date                            as date,
+    FORMAT_DATE('%Y', date)         as year,
+    FORMAT_DATE('%m', date)         as month,
+    FORMAT_DATE('%d', date)         as day,
+    FORMAT_DATE('%A' , date)        as weekday,
 from all_date
